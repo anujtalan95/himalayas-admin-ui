0d90896bd7c8bf4217d9ae449e837108972edc43
Anuj Talan <anuj25talan@gmail.com>
2020-05-25 22:30:24 +0530

    App Compponent - edit / view product
    App Compponent updated :
    - product component added
    - edit product component added
    - Product service added
    - route added for product and edit product
    - Bootstrap dashboard style added in app component html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
+import { AppComponent } from './app.component';
+import { EditProductComponent } from './components/edit-product/edit-product.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'products',
+    component:ProductComponent
+  },
+  {
+    path:'product/edit/:id',
+    component:EditProductComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductService } from './services/product.service';
+import { ProductComponent } from './components/product/product.component';
+import { EditProductComponent } from './components/edit-product/edit-product.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    EditProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
