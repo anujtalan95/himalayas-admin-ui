6df7e292d998981f25597ed3cfef5f2a3343da04
Anuj Talan <anuj25talan@gmail.com>
2020-05-28 22:34:03 +0530

    Delete product is added
    - user can delete the product
    Bug : product table not refresh after deleting the product

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -10,7 +11,7 @@ export class ProductComponent implements OnInit {
 
   public products;
 
-  constructor(private productService:ProductService) { }
+  constructor(private productService:ProductService, private router:Router) { }
 
   ngOnInit(): void {
     this.getProducts();
@@ -21,6 +22,16 @@ export class ProductComponent implements OnInit {
       data => {this.products = data},
       error => console.error(error),
       () =>  console.log('Product Loaded')
+    );
+  }
+
+  deleteProduct(productBody) {
+    this.productService.deleteProduct(productBody.id).subscribe(
+      data => {
+        this.router.navigate(['products']);
+      },
+      error => console.error(error),
+      () => console.log("Product Deleted")
     );
   }
 
