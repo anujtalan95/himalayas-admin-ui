951863e183f4dcbcc64feb17ae53f3fb15972846
Anuj Talan <anuj25talan@gmail.com>
2020-05-30 22:35:29 +0530

    Dashboard component added
    - Dashboard component added
    - Static charts are added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,14 @@ import { ProductComponent } from './components/product/product.component';
 import { AppComponent } from './app.component';
 import { EditProductComponent } from './components/edit-product/edit-product.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
   {
     path:'products',
     component:ProductComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductService } from './services/product.service';
 import { ProductComponent } from './components/product/product.component';
 import { EditProductComponent } from './components/edit-product/edit-product.component';
-import { AddProductComponent } from './components/add-product/add-product.component'
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     EditProductComponent,
-    AddProductComponent
+    AddProductComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ChartsModule 
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
